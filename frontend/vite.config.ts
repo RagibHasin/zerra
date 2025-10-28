1637a9b35ffdf0f3449f4fb917709cee867c0888
Muhammad Ragib Hasin <ragib.hasin@gmail.com>
2025-10-28 22:29:36 +0600

    fix error on edit
    downgraded rolldown-vite to 7.1.18

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,19 +4,20 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { compression } from 'vite-plugin-compression2';
 
+const plugins = [
+  tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+  react(),
+  tailwindcss(),
+];
 const prod = {
-  plugins: [
-    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
-    react(),
-    tailwindcss(),
-    compression({ algorithms: [['gzip', { level: 9 }], 'brotli'] }),
-  ],
+  plugins: [...plugins, compression({ algorithms: [['gzip', { level: 9 }], 'brotli'] })],
   build: { reportCompressedSize: false },
+  experimental: { enableNativePlugin: true },
 };
 const dev = {
-  plugins: [tanstackRouter({ target: 'react', autoCodeSplitting: true }), react(), tailwindcss()],
+  plugins,
   build: { sourcemap: true, minify: false, cssMinify: false, reportCompressedSize: false },
   experimental: { enableNativePlugin: true },
 };
 // https://vite.dev/config/
-export default defineConfig(dev);
+export default defineConfig(prod);
